6587466ca95433a279b5c8bdd0d45a38d2c54721
Cris2427 <alonso.ctp@gmail.com>
2026-06-11 11:05:06 -0400

    se configura MySQL, Swagger y el perfil de test con H2 para que funcione en local

--- src/main/java/com/rednorte/ms_pacientes/repository/PacienteRepository.java
+++ src/main/java/com/rednorte/ms_pacientes/repository/PacienteRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 
-    Optional<Paciente> findByNome(String rut);
+    Optional<Paciente> findByRut(String rut);
 
     boolean existsByRut(String rut);
 }
